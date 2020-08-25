9845e79bf3c0993d11060ed835ff430be5c98f2b
vidurshan <vidurshan1996>
2020-08-25 21:00:55 +0530

    login created

--- app/src/main/java/com/example/eticket/MainActivity.java
+++ app/src/main/java/com/example/eticket/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.eticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"database connected",Toast.LENGTH_SHORT).show();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
     }
 }
---
+++ app/src/main/java/com/example/eticket/member.java
@@ -0,0 +1,61 @@
+package com.example.eticket;
+
+public class member {
+
+    String username;
+    String name;
+    String  email;
+    String num;
+    String password;
+
+    public member()
+    {
+
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+}
