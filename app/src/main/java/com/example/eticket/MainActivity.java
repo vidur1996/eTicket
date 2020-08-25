41e0aa19483c012e1287b3a66dbe7ae3b6741dd0
vidurshan <vidurshan1996>
2020-08-25 21:49:05 +0530

    databse connected to login signup

--- app/src/main/java/com/example/eticket/MainActivity.java
+++ app/src/main/java/com/example/eticket/MainActivity.java
@@ -1,26 +1,98 @@
 package com.example.eticket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+Button signup_btn,login_btn;
+TextView user_txt,pass_txt;
+    String pass_db;
+    DatabaseReference reff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toast.makeText(MainActivity.this,"database connected",Toast.LENGTH_SHORT).show();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        signup_btn = findViewById(R.id.sign_btn);
+        login_btn  =findViewById(R.id.login_btn);
+        user_txt = findViewById(R.id.username_txt);
+        pass_txt = findViewById(R.id.password_txt);
 
-        myRef.setValue("Hello, World!");
 
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(),signup.class);
+                startActivity(it);
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = user_txt.getText().toString().trim();
+                String pass = pass_txt.getText().toString().trim();
+
+                if(username=="" || username.equals(""))
+                {
+                    Toast.makeText(MainActivity.this,"enter username",Toast.LENGTH_SHORT).show();
+                }
+                else if(pass=="" ||pass.equals(""))
+                {
+                    Toast.makeText(MainActivity.this,"enter username",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    reff = FirebaseDatabase.getInstance().getReference().child("member").child(username);
+                    reff.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            pass_db = dataSnapshot.child("password").getValue().toString();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Toast.makeText(MainActivity.this,"wrong username",Toast.LENGTH_SHORT).show();
+                            pass_txt.setText("");
+                            user_txt.setText("");
+                        }
+                    });
+
+
+                    if(pass.equals(pass_db) || pass==pass_db)
+                    {
+
+                        Toast.makeText(MainActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+                        pass_txt.setText("");
+                        user_txt.setText("");
+                        Intent star = new Intent(getApplicationContext(),main_menu.class);
+                        startActivity(star);
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this,"wrong password",Toast.LENGTH_SHORT).show();
+                        pass_txt.setText("");
+                    }
+                }
+
+
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/eticket/signup.java
@@ -0,0 +1,146 @@
+package com.example.eticket;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.widget.Toast.makeText;
+
+public class signup extends AppCompatActivity {
+    Button submit_btn , clear_btn , back_btn;
+    TextView name_txt, user_txt, pass1_txt,pass2_txt, mail_txt ,phone_txt  ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+        submit_btn = (Button)   findViewById(R.id.submit_btn);
+        clear_btn  = (Button)   findViewById(R.id.clear_btn );
+        back_btn   = (Button)   findViewById(R.id.back__btn );
+        name_txt   = (TextView) findViewById(R.id.name_txt  );
+        user_txt   = (TextView) findViewById(R.id.user_txt  );
+        pass1_txt  = (TextView) findViewById(R.id.pass1_txt );
+        pass2_txt  = (TextView) findViewById(R.id.pass2_txt );
+        mail_txt   = (TextView) findViewById(R.id.mail_txt  );
+        phone_txt  = (TextView) findViewById(R.id.phone_txt );
+
+        clear_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pass1_txt.setText("");
+                pass2_txt.setText("");
+                name_txt.setText("");
+                user_txt.setText("");
+                mail_txt.setText("");
+                phone_txt.setText("");
+            }
+        });
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(it);
+            }
+        });
+
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                String pass1 = pass1_txt.getText().toString().trim();
+                String pass2 = pass2_txt.getText().toString().trim();
+                String name = name_txt.getText().toString().trim();
+                String user = user_txt.getText().toString().trim();
+                String email= mail_txt.getText().toString().trim();
+                String phone = phone_txt.getText().toString().trim();
+                member mem = new member();
+                DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("member");
+
+                DatabaseReference.CompletionListener complete = new DatabaseReference.CompletionListener() {
+                    @Override
+
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            if(databaseError != null)
+                            {
+                                Toast.makeText(signup.this, "register failed", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(signup.this, "registration succesful", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    };
+
+                if(name.equals("") || email.equals("") || phone.equals("")) {
+                    if (name.equals(""))
+                    {
+                        makeText(signup.this, "enter the name", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (email.equals(""))
+                    {
+                        makeText(signup.this, "enter the email address", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(phone.equals(""))
+                    {
+                        makeText(signup.this, "enter the phone number", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+                else if( user.equals("")  )
+                {
+                    makeText(signup.this, "enter username", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+                else if(pass1.equals("") ||pass2.equals("") )
+                {
+                    makeText(signup.this, "enter passsword", Toast.LENGTH_SHORT).show();
+                }
+
+
+                else  if( ! pass1.equals(pass2)) {
+                    makeText(signup.this, "both passsword does not match", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+
+
+
+                else
+                {
+
+                    mem.setName(name);
+                    mem.setEmail(email);
+                    mem.setNum(phone);
+                    mem.setUsername(user);
+                    mem.setPassword(pass1);
+                    reff.child(user).setValue(mem ,complete);
+
+
+                }
+            }
+        });
+
+
+
+    }
+}
